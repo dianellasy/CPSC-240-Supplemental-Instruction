3bbe63087758af3102119eb4ff64cf58ca906bbb
Dianella Sy <dianellabsy@csu.fullerton.edu>
2025-06-18 16:10:21 -0700

    Updated r.sh

--- hello_world/r.sh
+++ hello_world/r.sh
@@ -8,13 +8,13 @@ echo "Remove old executable files if there are any"
 rm *.out
 
 echo "Assemble the X86 file hello.asm"
-nasm -f elf64 -l hello.lis -o hello.o hello.asm
+nasm -f elf64 -o hello.o hello.asm
 
 echo "Compile the C file main.c"
 gcc -c -m64 -Wall -fno-pie -no-pie -o main.o main.c
 
-echo "Link the two 'O' files main.o hello.o"
-gcc -m64 -o go.out hello.o main.o -fno-pie -no-pie
+echo "Link the two 'o' files main.o hello.o"
+gcc -m64 -Wall -fno-pie -no-pie -z noexecstack -lm -o go.out main.o hello.o  
 
 echo "Next """" will run"
 ./go.out
