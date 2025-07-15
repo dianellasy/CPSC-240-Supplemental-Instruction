7520e019323d1ea6887b98a785642afb39c93f1e
Dianella Sy <dianellabsy@csu.fullerton.edu>
2025-07-15 15:29:52 -0700

    Update README.md and Remove * from License

--- macro/area.asm
+++ macro/area.asm
@@ -1,9 +1,9 @@
 ; Program Name: "". This program demonstrates.  Copyright (C) 2025  Dianella Sy
 
-; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  *
-; version 3 as published by the Free Software Foundation.                                                                    *
-; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         *
-; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.      *
+; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  
+; version 3 as published by the Free Software Foundation.                                                                    
+; This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied         
+; warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.      
 ; A copy of the GNU General Public License v3 is available here: <https://www.gnu.org/licenses/>.
 
 ; Author Information:
