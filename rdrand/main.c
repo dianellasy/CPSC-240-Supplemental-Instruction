9e9a244212fc197e9bd41dcbd1e61111f21da679
Dianella Sy <dianellabsy@csu.fullerton.edu>
2025-11-04 19:05:11 -0800

    Rdrand

---
+++ rdrand/main.c
@@ -0,0 +1,53 @@
+// Program Name: "". This program demonstrates.  Copyright (C) 2025  Dianella Sy
+
+// This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License  
+// version 3 as published by the Free Software Foundation.                                                                    
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied        
+// warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.      
+// A copy of the GNU General Public License v3 is available here: <https://www.gnu.org/licenses/>.
+
+// Author Information:
+// Name:
+// CWID:
+// Section: CPSC 240-#
+// Email: @csu.fullerton.edu
+
+// Program Information:
+// Name:
+// Languages:
+// Start Date:
+// Completion Date:
+
+// Program's Purpose:
+// The "" will teach/introduce.
+
+// Project Information:
+// Files: 
+// Developing Computer: 
+// Status:
+
+// Translator Information:
+// GNU Compiler:
+// GNU Linker:
+
+// Execution:
+// ./go.out
+
+#include <stdio.h>
+
+extern int rdrands();
+
+int main(int argc, char* argv[]) {
+
+    printf("Welcome to Dianella's SI Session! 🤗\n");
+    
+    int return_rdrand = rdrands();
+
+    printf("\nYour random number is %d\n", return_rdrand);
+    
+    printf("\nA zero will be returned to the operating system.\n");
+
+    printf("Thank you for attending my SI session! I hope this helped 🩷\n");
+
+    return 0;
+}
