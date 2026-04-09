6c15970114038919482756a51df0d5b4b8d4a12b
Dianella Sy <dianellabsy@csu.fullerton.edu>
2026-04-09 11:15:05 -0700

    Pure Assembly Program

--- pure_assembly/r.sh
+++ pure_assembly/r.sh
@@ -10,14 +10,17 @@ rm *.out
 echo "Assemble the X86 file atof.asm"
 nasm -f elf64 -o atof.o atof.asm
 
+echo "Assemble the X86 file ftoa.asm"
+nasm -f elf64 -o ftoa.o ftoa.asm
+
 echo "Assemble the X86 file multiply.asm"
 nasm -f elf64 -o multiply.o multiply.asm
 
 echo "Assemble the X86 file pure_assembly.asm"
 nasm -f elf64 -o pure_assembly.o pure_assembly.asm
 
-echo "Link the three 'O' file atof.o multiply.o pure_assembly.o"
-ld -o go.out atof.o multiply.o pure_assembly.o -g
+echo "Link the four 'O' file atof.o ftoa.o multiply.o pure_assembly.o"
+ld -o go.out atof.o ftoa.o multiply.o pure_assembly.o -g
 
 echo "Next ""Pure Assembly: Input, Output & Multiply"" will run"
 ./go.out
